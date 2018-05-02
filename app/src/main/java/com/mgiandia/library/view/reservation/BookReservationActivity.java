55919334f6abdf56b9a4f45468ccd94957c1a6d0
Bill <info@coffeefindsyou.com>
2018-05-02 14:34:32 +0300

    BookReservationActivity ConstraintLayout

---
+++ app/src/main/java/com/mgiandia/library/view/reservation/BookReservationActivity.java
@@ -0,0 +1,15 @@
+package com.mgiandia.library.view.reservation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mgiandia.library.R;
+
+public class BookReservationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_reservation);
+    }
+}
