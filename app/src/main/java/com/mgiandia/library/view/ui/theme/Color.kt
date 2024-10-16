620ef882521228f0fee39f1e4a81860947130550
Vassilis Zafeiris <bzafiris@gmail.com>
2024-10-16 23:08:06 +0300

    Migrated theme (partially) to compose

---
+++ app/src/main/java/com/mgiandia/library/view/ui/theme/Color.kt
@@ -0,0 +1,29 @@
+package com.mgiandia.library.view.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// top android bar color, usually black
+val topBar = Color(0xff111111)
+// buttons and header color
+val colorPrimary = Color(0xffe84d3c)
+// background color
+val colorDarkBG = Color(0xff393939)
+// primary text color, used a ton on text views and titles
+val colorWhite = Color(0xffFFFFFF)
+// secondary text color, used a ton next to primary
+val colorSemiWhite = Color(0xffCACACA)
+// spinner default view color, can be same as colorSemiWhite
+val colorMediumSemiWhite = Color(0xffCACACA)
+
+// bellow are colors of small images
+val colorSmallBlue = Color(0xff0067b4)
+val colorSmallGreen = Color(0xff1EA33E)
+val colorSmallOrange = Color(0xfff8a100)
