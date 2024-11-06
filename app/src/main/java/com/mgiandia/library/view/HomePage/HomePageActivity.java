e767ce0505f2d57f21692a74f4ad7fe65a69f767
PavlosFas13 <coolpaul2017@gmail.com>
2024-11-06 17:38:42 +0200

    Home page
    I made the home page, but I don't know how to find the button from java, and set onClickListener to send it to kotlin class.

--- app/src/main/java/com/mgiandia/library/view/HomePage/HomePageActivity.java
+++ app/src/main/java/com/mgiandia/library/view/HomePage/HomePageActivity.java
@@ -3,6 +3,7 @@ package com.mgiandia.library.view.HomePage;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
